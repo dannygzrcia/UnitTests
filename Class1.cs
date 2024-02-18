cbb35184d50857415fd68dc21458ca4cf511d9ce
dannygzrcia <dannygzrcia>
2024-02-18 17:45:08 -0500

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MinimumLibrary
+{
+    public class MinimumCalculator
+    {
+        public static int Min(int a, int b)
+        {
+            return Math.Min(a, b);
+        }
+
+        public static int Min(int a, int b, int c)
+        {
+            return Math.Min(Math.Min(a, b), c);
+        }
+
+        public static double Min(double a, double b, double c)
+        {
+            return Math.Min(Math.Min(a, b), c);
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MinimumLibrary;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
